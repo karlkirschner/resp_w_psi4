d24093d67f9578ca229f9e5d82d330fc7441a8ac
kkirsc2m <karl.kirschner@h-brs.de>
2026-03-30 15:40:54 +0200

    added methanol

---
+++ tests/methanol.py
@@ -0,0 +1,25 @@
+import os
+import pytest
+import sys
+
+import numpy as np
+import psi4
+
+if not os.path.basename(os.getcwd()) == 'tests':
+    pytest.exit("ERROR: This test suite must be run from within the /tests directory "
+                "due to relative path dependencies for .ini and .xyz files.")
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
+import driver as resp
+
+## Trying to reproduce https://doi.org/10.1021/acs.jctc.2c00230?urlappend=%3Fref%3DPDF&jav=VoR&rel=cite-as
+## Input geometry was optimized at B3LYP/6-311++G(d,p)
+
+charges = resp.resp('methanol.ini')
+
+print('Unrestrained Electrostatic Potential Charges')
+print(f'{charges[0]}\n')
+
+print('Restrained Electrostatic Potential (RESP) Charges')
+print(f'{charges[1]}\n')
